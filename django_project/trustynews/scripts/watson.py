744adbd4bf68dfea26a1046208ade657e6bc00a8
David Zhang <zhangh87@mcmaster.ca>
2017-01-28 13:43:02 -0500

    adds simple analysis with AlchemyLanguage API

--- django_project/trustynews/scripts/watson.py
+++ django_project/trustynews/scripts/watson.py
@@ -1,2 +1,36 @@
-def hi():
-    print "hi"
+import json
+from os.path import join, dirname
+from watson_developer_cloud import AlchemyLanguageV1
+
+api_key_alc='1983387b2fb355dca6bc2cf73437d3e4bbfa6dbf'
+
+def analyze(url):
+    alchemy_language = AlchemyLanguageV1(api_key=api_key_alc)
+    combined_operations = [ 'entity', 'keyword', 'title', 'concept', 'doc-emotion']
+    global data
+    data = alchemy_language.combined(url=url, extract=combined_operations)
+    
+
+def getTitle():
+    global data
+    title = data['title']
+    return title.encode('ascii','ignore')
+
+def getKeywords():
+    global data
+    return data['keywords']    
+
+def getKeyKeywords():
+    words = []
+    global data
+    for i in range(0,7):
+        words.append(data['keywords'][i]['text'])
+    return words
+    
+def getEmotions():
+    global data
+    emotions= data['docEmotions']
+    return emotions
+
+
+
