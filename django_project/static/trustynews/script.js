5fc5a2a2fb4367e3c37c2c3ed975f2ac15a97e4e
jack li <jackyanart@gmail.com>
2017-01-28 18:47:44 -0500

    added url checking n loading

--- django_project/static/trustynews/script.js
+++ django_project/static/trustynews/script.js
@@ -1,4 +1,6 @@
-
+$(document).ready(function(){
+    $("button[name='go']").attr('disabled', false);
+});
 
 function setupML(json){
     $cols = $(".ml_cont .stats .col ul");
@@ -8,12 +10,15 @@ function setupML(json){
 
     for (var i = 0; i < $emotion.length; i++) {
         $($emotion[i]).find("div").css("width",json.Emotion[$($emotion[i]).find("span")[0].innerHTML]*100 + "%");
+        $($emotion[i]).find(".percent").css("width",json.Emotion[$($emotion[i]).find("span")[0].innerHTML]*100 + "%");
     }
     for (var i = 0; i < $lang.length; i++) {
         $($lang[i]).find("div").css("width",json.Language[$($lang[i]).find("span")[0].innerHTML]*100 + "%");
+        $($lang[i]).find(".percent").css("width",json.Language[$($lang[i]).find("span")[0].innerHTML]*100 + "%");
     }
     for (var i = 0; i < $social.length; i++) {
         $($social[i]).find("div").css("width",json.Social[$($social[i]).find("span")[0].innerHTML]*100 + "%");
+        $($social[i]).find(".percent").css("width",json.Social[$($social[i]).find("span")[0].innerHTML]*100 + "%");
     }
 
     total = $(".ml_cont .overalltotal");
@@ -28,4 +33,19 @@ function setupML(json){
         str += "<li>"+json.Keywords[i]+"</li>";
     }
     keywords.html(str);
+}
+
+
+function changeTab(tab){
+    $(".tabs_cont li").removeClass("selected");
+    $($(".tabs_cont li")[tab]).addClass("selected");
+    $(".content_cont .cont").addClass("hide");
+    $($(".content_cont .cont")[tab]).removeClass("hide");
+}
+
+
+
+function isURL(str) {
+    var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
+   return regexp.test(str);
 }
