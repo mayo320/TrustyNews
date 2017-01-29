b150a05520cf001d6967061fae7a66ef7eced791
David Zhang <zhangh87@mcmaster.ca>
2017-01-28 19:32:12 -0500

    Added new function that evaluates subfunctions

---
+++ django_project/trustynews/scripts/Analyze.py
@@ -0,0 +1,22 @@
+import MLAnalysis
+import DatabaseSearch
+import search_api
+def analyze(url):
+    returnList= {}
+    returnList['MachineLearning'] =  MLAnalysis.calculate(url)
+    
+    if DatabaseSearch.is_valid(url):
+        returnList['DomainCheck'] = 1
+    elif DatabaseSearch.is_fake(url):
+        returnList['DomainCheck'] = 0
+    else:
+        returnList['DomainCheck'] = 2
+
+    stringList = []
+    for i in range(0,3):
+        stringList.append(returnList['MachineLearning']['Keywords'][i].encode('ascii','ignore'))
+
+    returnList['SearchResults'] = search_api.keyword_search(stringList.join(' '))
+    #returnList['TotalReliability'] = 
+    return returnList
+    
